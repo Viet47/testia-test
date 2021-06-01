ad23ee208cf27077355fd7e4eba580e706c25a87
Lilian PARISATO <lilian@MacBook-Pro-de-Lilian.local>
2021-06-01 23:33:31 +0200

    fix(test): Jest run only angular test

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,9 @@
 require('jest-preset-angular/ngcc-jest-processor');
 
 module.exports = {
-    preset: 'jest-preset-angular',
-    setupFilesAfterEnv: ['<rootDir>/setup-jest.ts', "jest-canvas-mock"],
-  };
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts', "jest-canvas-mock"],
+  testMatch: [
+    "<rootDir>/src/**/*.spec.ts"
+  ]
+};
